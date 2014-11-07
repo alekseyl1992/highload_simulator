223c62347361ec411bd0d818c23d0271daa33404
alekseyl1992 <alekseyl@list.ru>
2014-11-07 20:56:37 +0300

    Basic multiclient-server communication setup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import simpy
+import random
 
 
 class SimObj(object):
@@ -6,33 +7,91 @@ class SimObj(object):
         self.env = env
 
 
-class Server(SimObj):
-    def __init__(self, env, name, cores_count):
+class Message(object):
+    def __init__(self, env, source_id, response_pipe, text, size):
+        self.time = env.now
+        self.source_id = source_id
+        self.response_pipe = response_pipe
+        self.text = text
+        self.size = size
+
+
+class Client(SimObj):
+    def __init__(self, env, server_pipe, uplink_speed, downlink_speed, id):
         super().__init__(env)
-        self.name = name
+
+        self.client_pipe = simpy.Store(self.env)
+        self.server_pipe = server_pipe
+        self.type = "PC"
+        self.id = id
+        self.uplink_speed = uplink_speed
+        self.downlink_speed = downlink_speed
+
+        self.process = None
+
+    def start(self):
+        self.process = self.env.process(self.work())
+
+    def work(self):
+        while True:
+            idle_time = random.uniform(1, 10)
+            yield self.env.timeout(idle_time)
+
+            # send request
+            request = Message(self.env, self.id, self.client_pipe, "Hello, Server!", 10)
+            self.server_pipe.put(request)
+            print("[Client %d] Request sent" % self.id)
+
+            # wait for response
+            response = yield self.client_pipe.get()
+            print("[Client %d] Response: %s" % (self.id, response.text))
+
+
+class Server(SimObj):
+    def __init__(self, env, id, cores_count):
+        super().__init__(env)
+        self.id = id
         self.coresCount = cores_count
+        self.server_pipe = simpy.Store(self.env)
 
         self.processes = []
 
-    def start(self):
-        # fork
-        for pid in range(0, self.coresCount):
-            self.processes.append(env.process(self.work(pid)))
+    def get_pipe(self):
+        return self.server_pipe
 
-    def work(self, pid):
+    def start(self):
         print("Server started at %d" % env.now)
 
+        # fork
+        for pid in range(0, self.coresCount):
+            self.processes.append(self.env.process(self.work(pid)))
+
+    def work(self, pid):
         while True:
-            req_time = 10
+            # wait for request
+            request = yield self.server_pipe.get()
+
+            req_time = random.uniform(10, 20)
             yield env.timeout(req_time)
-            print("[Server: %d, pid: %d] Request handled at %d" % (self.name, pid, env.now))
+            print("[Server: %d, pid: %d] Request from %d: \"%s\" handled at %d"
+                  % (self.id, pid, request.source_id, request.text, env.now))
+
+            # send response
+            response = Message(self.env, self.id, None, "Hello, Client! from %d to %d!" % (self.id, request.source_id), 20)
+            request.response_pipe.put(response)
 
 
 if __name__ == "__main__":
     env = simpy.Environment()
 
-    for i in range(0, 10):
-        server = Server(env, i, 4)
-        server.start()
+    server = Server(env, 1, 2)
+    server.start()
 
-    env.run(until=30)
+    client1 = Client(env, server.get_pipe(), 10, 20, 1)
+    client1.start()
+
+    client2 = Client(env, server.get_pipe(), 10, 20, 2)
+    client2.start()
+
+
+    env.run(until=100)
